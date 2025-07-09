5dfede37cdaf8d3d096fa16c796b79c4a3bff9ed
jyroball <jyrojimenez@gmail.com>
2025-07-08 23:00:26 -0700

    Changed profile screen as start screen with user cards and arrays

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,51 +1,47 @@
 import 'package:flutter/material.dart';
 //Different screens for bio
-import 'package:test1/screens/start_screen.dart';
+//import 'package:test1/screens/start_screen.dart';
 import 'package:test1/screens/bio_screen.dart';
+//import model
+import 'package:test1/models/user.dart';
+//import reusable widghets
+import 'package:test1/widgets/user_card.dart';
 
 class Profile extends StatefulWidget {
   const Profile({super.key});
 
   //no build method if stateful widget
   @override
-  State<Profile> createState() {
-    return _ProfileState();
-  }
-
+  State<Profile> createState() => _ProfileState();
 }
 
 // "_" means that class is a private class for the file
 class _ProfileState extends State<Profile> {
-  //nullable widget for now when intializing
-  Widget? activeScreen;
+  //have an array for the list of users
+  final List<User> users = [];
 
-  //boolean to know what screen we should be in
-  bool isStart = true;
+  //
+  //  NEED TO MAKE A ADD USER FORM LATER
+  //
 
-  //Starts an initialization logic when objects are created
-  @override
-  void initState() {
-    //change widets, make sure variable is a widget since var sets it as a StartScreen object
-    activeScreen = StartScreen(switchScreen);
-    super.initState();
+  //Using user card button view profile
+  void _viewBio(User user) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (_) => BioScreen(user: user)),
+    );
   }
 
-  //Switch between bio nad more information
-  void switchScreen() {
-    setState(() {
-      //toggle boolean when button poreesesd
-      isStart = !isStart;
-    });
-  }
-
+  //just output cards of users
   @override
   Widget build(context) {
-    //change screeen when re rendered
-    activeScreen = isStart ? StartScreen(switchScreen) : BioScreen(switchScreen);
-
-    //change to just returning screen since we have theme to control design now
     return Scaffold(
-      body: activeScreen,
+      appBar: AppBar(title: const Text('Users')),
+      body: ListView.builder(
+        itemCount: users.length,
+        //similar top old project, can prob add id to user card and make cards dismissable
+        itemBuilder: (ctx, i) => UserCard(user: users[i], goBio: () => _viewBio(users[i])),
+      ),
     );
   }
 }
