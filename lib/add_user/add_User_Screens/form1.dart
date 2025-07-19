82a06d89bdce662a53dbfed4713f997bf2a86b74
jyroball <jyrojimenez@gmail.com>
2025-07-18 19:01:02 -0700

    changed form screens to hold bloc values instead of local values

--- lib/add_user/add_User_Screens/form1.dart
+++ lib/add_user/add_User_Screens/form1.dart
@@ -1,162 +1,185 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+//bloc imports
+import '../bloc/add_user_bloc.dart';
+import '../bloc/add_user_event.dart';
+import '../bloc/add_user_state.dart';
 
 class FormOne extends StatelessWidget {
   //variables to send back to add_user page
   final GlobalKey<FormState> formKey;
-  final Function(String) newFirstName;
-  final Function(String) newLastName;
-  final DateTime? newBirthdate;
-  final int newAge;
-  final Function(DateTime) newBirthDateCall;
-  final Function(String) newOccuputaion;
-  final Function(String) newBio;
+  // final Function(String) newFirstName;
+  // final Function(String) newLastName;
+  // final DateTime? newBirthdate;
+  // final int newAge;
+  // final Function(DateTime) newBirthDateCall;
+  // final Function(String) newOccuputaion;
+  // final Function(String) newBio;
 
   //naviagtion variables
-  final VoidCallback next;
+  // final VoidCallback next;
 
   //passed vars
-  const FormOne({
-    super.key,
-    required this.formKey,
-    required this.newFirstName,
-    required this.newLastName,
-    required this.newBirthdate,
-    required this.newAge,
-    required this.newBirthDateCall,
-    required this.newOccuputaion,
-    required this.newBio,
-    required this.next,
-  });
+  // const FormOne({
+  //   super.key,
+  //   required this.formKey,
+  //   required this.newFirstName,
+  //   required this.newLastName,
+  //   required this.newBirthdate,
+  //   required this.newAge,
+  //   required this.newBirthDateCall,
+  //   required this.newOccuputaion,
+  //   required this.newBio,
+  //   required this.next,
+  // });
+
+  const FormOne({super.key, required this.formKey});
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      child: SafeArea(
-        child: Column(
-          children: [
+    return BlocBuilder<UserBloc, UserState>(
+      builder: (context, state) {
+        final user = state.user;
+        final currentStep = state.currentStep + 1;
 
-            //Text Form Inputs
-            Expanded(
-              child: SingleChildScrollView(
-                padding: const EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    //Output instructions at the top
-                    const Text("1 out of 3", style: TextStyle(color: Colors.grey)),
-                    const SizedBox(height: 8),
-                    const Text("Personal Information", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                    const Text("Input your personal information. All fields are required.",
-                        style: TextStyle(color: Colors.grey)),
-              
-                    //Output First name Input box
-                    const SizedBox(height: 16),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: 'First Name',
-                        hintText: 'John',
-                      ),
-                      validator: (val) => val == null || val.isEmpty ? 'First Name is required' : null,
-                      onSaved: (val) => newFirstName(val!),
-                    ),
-              
-                    //Output Last name input box
-                    const SizedBox(height: 12),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: 'Last Name',
-                        hintText: 'Doe',
-                      ),
-                      validator: (val) => val == null || val.isEmpty ? 'Last Name is required' : null,
-                      onSaved: (val) => newLastName(val!),
-                    ),
-                    
-                    //Output Birthdate picker and Get Age
-                    const SizedBox(height: 12),
-                    Row(
+        return Form(
+          key: formKey,
+          child: SafeArea(
+            child: Column(
+              children: [
+        
+                //Text Form Inputs
+                Expanded(
+                  child: SingleChildScrollView(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Expanded(
-                          flex: 2,
-                          child: TextFormField(
-                            readOnly: true,
-                            decoration: const InputDecoration(labelText: 'Birthdate'),
-                            controller: TextEditingController(
-                              text: newBirthdate != null
-                                  ? '${newBirthdate!.month.toString().padLeft(2, '0')}/${newBirthdate!.day.toString().padLeft(2, '0')}/${newBirthdate!.year}'
-                                  : '',
+                        //Output instructions at the top
+                        Text("${currentStep} out of 3", style: const TextStyle(color: Colors.grey)),
+                        const SizedBox(height: 8),
+                        const Text("Personal Information", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                        const Text("Input your personal information. All fields are required.",
+                            style: TextStyle(color: Colors.grey)),
+                  
+                        //Output First name Input box
+                        const SizedBox(height: 16),
+                        TextFormField(
+                          initialValue: user.firstName,
+                          decoration: const InputDecoration(
+                            labelText: 'First Name',
+                            hintText: 'John',
+                          ),
+                          validator: (val) => val == null || val.isEmpty ? 'First Name is required' : null,
+                          //onSaved: (val) => newFirstName(val ?? ''),
+                          onChanged: (val) => context.read<UserBloc>().add(UpdateUserField(firstName: val)),
+                        ),
+                  
+                        //Output Last name input box
+                        const SizedBox(height: 12),
+                        TextFormField(
+                          initialValue: user.lastName,
+                          decoration: const InputDecoration(
+                            labelText: 'Last Name',
+                            hintText: 'Doe',
+                          ),
+                          validator: (val) => val == null || val.isEmpty ? 'Last Name is required' : null,
+                          //onSaved: (val) => newLastName(val!),
+                          onChanged: (val) => context.read<UserBloc>().add(UpdateUserField(lastName: val)),
+                        ),
+                        
+                        //Output Birthdate picker and Get Age
+                        const SizedBox(height: 12),
+                        Row(
+                          children: [
+                            Expanded(
+                              flex: 2,
+                              child: TextFormField(
+                                readOnly: true,
+                                decoration: const InputDecoration(labelText: 'Birthdate'),
+                                controller: TextEditingController(
+                                  text: user.birthDate != null
+                                      ? '${user.birthDate.month.toString().padLeft(2, '0')}/${user.birthDate.day.toString().padLeft(2, '0')}/${user.birthDate.year}'
+                                      : '',
+                                ),
+                                //show calendar to pick date
+                                onTap: () async {
+                                  final picked = await showDatePicker(
+                                    context: context,
+                                    initialDate: DateTime.now(),
+                                    firstDate: DateTime(1925),
+                                    lastDate: DateTime.now(),
+                                  );
+                                  if (picked != null) {
+                                    context.read<UserBloc>().add(UpdateUserField(birthDate: picked));
+                                  }
+                                },
+                                validator: (_) => user.birthDate == null ? 'Birthdate is required' : null,
+                              ),
                             ),
-                            //show calendar to pick date
-                            onTap: () async {
-                              final picked = await showDatePicker(
-                                context: context,
-                                initialDate: DateTime.now(),
-                                firstDate: DateTime(1925),
-                                lastDate: DateTime.now(),
-                              );
-                              if (picked != null) {
-                                newBirthDateCall(picked);
-                              }
-                            },
-                            validator: (_) => newBirthdate == null ? 'Birthdate is required' : null,
-                          ),
+        
+                            const SizedBox(width: 12),
+        
+                            Expanded(
+                              flex: 1,
+                              child: TextFormField(
+                                readOnly: true,
+                                decoration: const InputDecoration(labelText: 'Age'),
+                                controller: TextEditingController(text: user.age.toString()),
+                              ),
+                            ),
+                          ],
                         ),
-
-                        const SizedBox(width: 12),
-
-                        Expanded(
-                          flex: 1,
-                          child: TextFormField(
-                            readOnly: true,
-                            decoration: const InputDecoration(labelText: 'Age'),
-                            controller: TextEditingController(text: newAge.toString()),
+        
+                        //Output Occupation Input
+                        const SizedBox(height: 12),
+                        TextFormField(
+                          initialValue: user.occupation,
+                          decoration: const InputDecoration(
+                            labelText: 'Occupation',
+                            hintText: 'Job Title',
                           ),
+                          validator: (val) => val == null || val.isEmpty ? 'Occupation is required' : null,
+                          //onSaved: (val) => newOccuputaion(val!),
+                          onChanged: (val) => context.read<UserBloc>().add(UpdateUserField(occupation: val)),
                         ),
+        
+                        //Output Bio Input
+                        const SizedBox(height: 12),
+                        TextFormField(
+                          initialValue: user.bio,
+                          decoration: const InputDecoration(
+                            labelText: 'Biography',
+                            hintText: 'Describe yourself, what do you do, who are you?',
+                          ),
+                          maxLines: 5,
+                          validator: (val) => val == null || val.isEmpty ? 'Biography is required' : null,
+                          //onSaved: (val) => newBio(val!),
+                          onChanged: (val) => context.read<UserBloc>().add(UpdateUserField(bio: val)),
+                        ),
+                        
                       ],
                     ),
-
-                    //Output Occupation Input
-                    const SizedBox(height: 12),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: 'Occupation',
-                        hintText: 'Job Title',
-                      ),
-                      validator: (val) => val == null || val.isEmpty ? 'Occupation is required' : null,
-                      onSaved: (val) => newOccuputaion(val!),
-                    ),
-
-                    //Output Bio Input
-                    const SizedBox(height: 12),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: 'Biography',
-                        hintText: 'Describe yourself, what do you do, who are you?',
-                      ),
-                      maxLines: 5,
-                      validator: (val) => val == null || val.isEmpty ? 'Biography is required' : null,
-                      onSaved: (val) => newBio(val!),
-                    ),
-                    
-                  ],
+                  ),
                 ),
-              ),
+        
+                //Next Button
+                // Padding(
+                //   padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+                //   child: SizedBox(
+                //     width: double.infinity,
+                //     child: ElevatedButton(
+                //       onPressed: next,
+                //       child: const Text("Next", style: TextStyle(fontSize: 16)),
+                //     ),
+                //   ),
+                // ),
+              ],
             ),
-
-            //Next Button
-            Padding(
-              padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
-              child: SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: next,
-                  child: const Text("Next", style: TextStyle(fontSize: 16)),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+          ),
+        );
+      }
     );
   }
 }
--- lib/add_user/add_User_Screens/form3.dart
+++ lib/add_user/add_User_Screens/form3.dart
@@ -1,175 +1,193 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
+
+//bloc imports
+import '../bloc/add_user_bloc.dart';
+import '../bloc/add_user_event.dart';
+import '../bloc/add_user_state.dart';
 
 final formatter = DateFormat.yMd();
 
 class FormThree extends StatelessWidget {
-  final String firstName;
-  final String lastName;
-  final DateTime birthDate;
-  final int age;
-  final String occupation;
-  final String bio;
-  final String email;
-  final VoidCallback back;
-  final VoidCallback submit;
+  // final String firstName;
+  // final String lastName;
+  // final DateTime birthDate;
+  // final int age;
+  // final String occupation;
+  // final String bio;
+  // final String email;
+  // final VoidCallback back;
+  // final VoidCallback submit;
 
-  const FormThree({
-    super.key,
-    required this.firstName,
-    required this.lastName,
-    required this.birthDate,
-    required this.age,
-    required this.occupation,
-    required this.bio,
-    required this.email,
-    required this.back,
-    required this.submit,
-  });
+  // const FormThree({
+  //   super.key,
+  //   required this.firstName,
+  //   required this.lastName,
+  //   required this.birthDate,
+  //   required this.age,
+  //   required this.occupation,
+  //   required this.bio,
+  //   required this.email,
+  //   required this.back,
+  //   required this.submit,
+  // });
+
+  const FormThree({super.key});
 
   //Helper function to output foratted date
-  String get formattedDate {
-    return formatter.format(birthDate);
-  }
+  // String get formattedDate {
+  //   return formatter.format(user.birthDate);
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        children: [
-          Expanded(
-            child: SingleChildScrollView(
-              padding: const EdgeInsets.all(20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  //Output instructions at the top
-                  const Text("3 out of 3", style: TextStyle(color: Colors.grey)),
-                  const SizedBox(height: 8),
-                  const Text("Review Information", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                  const Text("Please review all of your information before you continue.", style: TextStyle(color: Colors.grey)),
-                  
-                  //Output all information in rows
-                  //Can probably make reusable widgets for these later????
-                  const SizedBox(height: 40),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Expanded(
-                        flex: 1,
-                        child: Text("First Name: ", style: TextStyle(fontWeight: FontWeight.bold))
-                      ),
-                      Expanded(
-                        flex: 2,
-                        child: Text(firstName),
-                      ),
-                    ],
-                  ),
+    return BlocBuilder<UserBloc, UserState>(
+      builder: (context, state) {
+        final user = state.user;
+        final currentStep = state.currentStep + 1;
 
-                  const SizedBox(height: 16),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Expanded(
-                        flex: 1,
-                        child: Text("Last Name: ", style: TextStyle(fontWeight: FontWeight.bold))
-                      ),
-                      Expanded(
-                        flex: 2,
-                        child: Text(lastName),
-                      ),
-                    ],
-                  ),
+        //use this instead since user can't be accessed by helper function
+        String formattedDate = formatter.format(user.birthDate);
 
-                  const SizedBox(height: 16),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Expanded(
-                        flex: 1,
-                        child: Text("Birth Date: ", style: TextStyle(fontWeight: FontWeight.bold))
-                      ),
-                      Expanded(
-                        flex: 2,
-                        child: Text(formattedDate)
-                      ),
-                    ],
-                  ),
-
-                  const SizedBox(height: 16),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Expanded(
-                        flex: 1,
-                        child: Text("Age: ", style: TextStyle(fontWeight: FontWeight.bold))
-                      ),
-                      Expanded(
-                        flex: 2,
-                        child: Text(age.toString())
-                      ),
-                    ],
-                  ),
-
-                  const SizedBox(height: 16),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Expanded(
-                        flex: 1,
-                        child: Text("Email: ", style: TextStyle(fontWeight: FontWeight.bold))
-                      ),
-                      Expanded(
-                        flex: 2,
-                        child: Text(email),
-                      ),
-                    ],
-                  ),
-
-                  const SizedBox(height: 40),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text("Biography - Describe yourself: ", style: TextStyle(fontWeight: FontWeight.bold)),
-                      Text(bio),
-                    ],
-                  ),
-                  
-                ],
-              ),
-            ),
-          ),
-
-          //Next Button
-          Row(
+        return SafeArea(
+          child: Column(
             children: [
-              //Back Button
               Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
-                  child: ElevatedButton(
-                    onPressed: back,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromARGB(255, 154, 147, 147),
-                    ),
-                    child: const Text("Back", style: TextStyle(fontSize: 16)),
+                child: SingleChildScrollView(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      //Output instructions at the top
+                      Text("${currentStep} out of 3", style: const TextStyle(color: Colors.grey)),
+                      const SizedBox(height: 8),
+                      const Text("Review Information", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                      const Text("Please review all of your information before you continue.", style: TextStyle(color: Colors.grey)),
+                      
+                      //Output all information in rows
+                      //Can probably make reusable widgets for these later????
+                      const SizedBox(height: 40),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Expanded(
+                            flex: 1,
+                            child: Text("First Name: ", style: TextStyle(fontWeight: FontWeight.bold))
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: Text(user.firstName),
+                          ),
+                        ],
+                      ),
+        
+                      const SizedBox(height: 16),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Expanded(
+                            flex: 1,
+                            child: Text("Last Name: ", style: TextStyle(fontWeight: FontWeight.bold))
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: Text(user.lastName),
+                          ),
+                        ],
+                      ),
+        
+                      const SizedBox(height: 16),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Expanded(
+                            flex: 1,
+                            child: Text("Birth Date: ", style: TextStyle(fontWeight: FontWeight.bold))
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: Text(formattedDate)
+                          ),
+                        ],
+                      ),
+        
+                      const SizedBox(height: 16),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Expanded(
+                            flex: 1,
+                            child: Text("Age: ", style: TextStyle(fontWeight: FontWeight.bold))
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: Text(user.age.toString())
+                          ),
+                        ],
+                      ),
+        
+                      const SizedBox(height: 16),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Expanded(
+                            flex: 1,
+                            child: Text("Email: ", style: TextStyle(fontWeight: FontWeight.bold))
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: Text(user.email),
+                          ),
+                        ],
+                      ),
+        
+                      const SizedBox(height: 40),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text("Biography - Describe yourself: ", style: TextStyle(fontWeight: FontWeight.bold)),
+                          Text(user.bio),
+                        ],
+                      ),
+                      
+                    ],
                   ),
                 ),
               ),
-              //Next Button
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
-                  child: ElevatedButton(
-                      onPressed: submit,
-                      child: const Text("Continue", style: TextStyle(fontSize: 16)),
-                  ),
-                ),
-              ),
-            ]
+        
+              // //Next Button
+              // Row(
+              //   children: [
+              //     //Back Button
+              //     Expanded(
+              //       child: Padding(
+              //         padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+              //         child: ElevatedButton(
+              //           onPressed: back,
+              //           style: ElevatedButton.styleFrom(
+              //             backgroundColor: const Color.fromARGB(255, 154, 147, 147),
+              //           ),
+              //           child: const Text("Back", style: TextStyle(fontSize: 16)),
+              //         ),
+              //       ),
+              //     ),
+              //     //Next Button
+              //     Expanded(
+              //       child: Padding(
+              //         padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+              //         child: ElevatedButton(
+              //             onPressed: submit,
+              //             child: const Text("Continue", style: TextStyle(fontSize: 16)),
+              //         ),
+              //       ),
+              //     ),
+              //   ]
+              // ),
+            ],
           ),
-        ],
-      ),
+        );
+      }
     );
   }
 }
